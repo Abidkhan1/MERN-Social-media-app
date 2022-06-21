305d749fac3f23b7e37629d06d9359c01364b815
Abid Khan <niazi.abid15@gmail.com>
2022-06-21 15:40:40 +0500

    Delete and Like functionality added

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
@@ -29,9 +30,40 @@ export const updatePost = async (req, res) => {
   const { id: _id } = req.params;
   const post = req.body;
 
-  if (!mongoose.Types.ObjectId.IsValid(_id))
+  if (!mongoose.Types.ObjectId.isValid(_id))
     return res.status(404).send("No data with this ID");
 
-  const updatedPost = PostMessage.findByIdAndUpdate(_id, post, { new: true });
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    _id,
+    { ...post, _id },
+    { new: true }
+  );
   res.json(updatedPost);
 }; //updatePost
+
+export const deletePost = async (req, res) => {
+  const { id: _id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("No data with this ID");
+
+  await PostMessage.findByIdAndDelete(_id);
+
+  return res.json({ message: "Post deleted successfuly." });
+};
+
+export const likePost = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("No data with this ID");
+
+  const post = await PostMessage.findById(id);
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    id,
+    { like: post.like + 1 },
+    { new: true }
+  );
+
+  return res.json(updatedPost);
+}; //likePost
--- index.js
+++ index.js
@@ -2,24 +2,24 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
-
+import dotenv from "dotenv";
 //Routes
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: "true" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: "true" }));
 app.use(cors());
-
 app.use("/posts", postRoutes);
-
-const CONNECTION_URL =
-  "mongodb+srv://abidkhan15:Arshadkhan@cluster0.2v4jy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
   )
